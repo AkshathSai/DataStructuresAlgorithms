833d945a40f1e2a1802e7797472824fa95762f08
Akshath Sai, Pittala <akshathsai.pittala@outlook.com>
2023-08-05 12:00:13 +0530

    Code Refactoring

--- src/main/java/org/example/algorithms/recursion/Factorial.java
+++ src/main/java/org/example/algorithms/recursion/Factorial.java
@@ -7,13 +7,13 @@ public class Factorial {
 
     public static void main(String[] args) {
 
-        LOGGER.info("Factorial value " + factorial(3));
+        LOGGER.info("Factorial value " + factorial(5));
 
     }
 
     private static int factorial(int n) {
 
-        if (n == 1) {
+        if (n == 1 || n == 0) {
             return 1;
         }
 
