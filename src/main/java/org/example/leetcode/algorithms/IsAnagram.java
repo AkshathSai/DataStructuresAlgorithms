51087aef44926c46721004b143f2361e3b5e99cd
Akshath Sai, Pittala <akshathsai.pittala@outlook.com>
2024-03-15 13:11:37 +0530

    Adding Anagram algo question

--- src/main/java/org/example/leetcode/algorithms/IsAnagram.java
+++ src/main/java/org/example/leetcode/algorithms/IsAnagram.java
@@ -16,8 +16,30 @@ public class IsAnagram {
         String s4 = "car";
 
         // call your methods here
-
+        log.info("Is Anagram? {}", isAnagramWithMap(s1, s2));
+        log.info("Is Anagram? {}", isAnagramWithMap(s3, s4));
+        log.info("Is Anagram? {}", isAnagramWithArray(s1, s2));
+        log.info("Is Anagram? {}", isAnagramWithArray(s3, s4));
+        log.info("Is Anagram? {}", isAnagramWithSortCheck(s1, s2));
+        log.info("Is Anagram? {}", isAnagramWithSortCheck(s3, s4));
     }
 
+    private static boolean isAnagramWithMap(String s1, String s2) {
+        // TODO
+
+        return false;
+    }
+
+    private static boolean isAnagramWithArray(String s1, String s2) {
+        // TODO
+
+        return false;
+    }
+
+    private static boolean isAnagramWithSortCheck(String s1, String s2) {
+        // TODO
+
+        return false;
+    }
 
 }
