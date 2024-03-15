e4b8094eb73dae5b69e3e31ae49862585c41af4d
Akshath Sai, Pittala <akshathsai.pittala@outlook.com>
2024-03-15 21:25:56 +0530

    Programmed IsAnagram algorithm using sort check

--- src/main/java/org/example/leetcode/algorithms/IsAnagram.java
+++ src/main/java/org/example/leetcode/algorithms/IsAnagram.java
@@ -2,6 +2,8 @@ package org.example.leetcode.algorithms;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Arrays;
+
 @Slf4j
 public class IsAnagram {
 
@@ -15,7 +17,6 @@ public class IsAnagram {
         String s3 = "rat";
         String s4 = "car";
 
-        // call your methods here
         log.info("Is Anagram? {}", isAnagramWithMap(s1, s2));
         log.info("Is Anagram? {}", isAnagramWithMap(s3, s4));
         log.info("Is Anagram? {}", isAnagramWithArray(s1, s2));
@@ -31,37 +32,47 @@ public class IsAnagram {
     }
 
     private static boolean isAnagramWithArray(String s, String t) {
-       
-    if(s.length()!=t.length()){
-        return false;
-        }
-      int arr[]=new int[26];
-        for(int i=0;i<arr.length;i++){
-            arr[i]=0;
-        }
-     char c[]=s.toCharArray();
-     char c1[]=t.toCharArray();
-     for(int i=0;i<c.length;i++){
-        arr[c[i]-'a']++;
 
-     }
-     for(int i=0;i<c.length;i++){
-      
-        arr[c1[i]-'a']--;
-     }
-    for(int a:arr){
-        if(a!=0){
-        return false;
+        if (s.length() != t.length()) {
+            return false;
         }
-         }
-    return true;
-}
+        int arr[] = new int[26];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = 0;
+        }
+        char c[] = s.toCharArray();
+        char c1[] = t.toCharArray();
+        for (int i = 0; i < c.length; i++) {
+            arr[c[i] - 'a']++;
+
+        }
+        for (int i = 0; i < c.length; i++) {
+
+            arr[c1[i] - 'a']--;
+        }
+
+        for (int a : arr) {
+            if (a != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
     
 
     private static boolean isAnagramWithSortCheck(String s1, String s2) {
-        // TODO
+        char[] charArray1 = s1.toCharArray();
+        Arrays.sort(charArray1);
+        s1 = new String(charArray1);
+        log.info("s1 {}", s1);
 
-        return false;
+        char[] charArray2 = s2.toCharArray();
+        Arrays.sort(charArray2);
+        s2 = new String(charArray2);
+        log.info("s2 {}", s2);
+
+        return s1.equals(s2);
     }
 
 }
