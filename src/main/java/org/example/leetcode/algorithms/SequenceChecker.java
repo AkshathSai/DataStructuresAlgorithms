4497fd9982cc12c5c11e27725e121bffdf6484e2
Akshath Sai Pittala <akshathsai.pittala@outlook.com>
2024-03-17 00:08:30 +0530

    Programmed isSubsequence checker Algorithm

--- src/main/java/org/example/leetcode/algorithms/SequenceChecker.java
+++ src/main/java/org/example/leetcode/algorithms/SequenceChecker.java
@@ -8,9 +8,26 @@ public class SequenceChecker {
     public static void main(String[] args) {
         log.info("{}", isSubsequence("abc", "ahbgdc"));
         log.info("{}", isSubsequence("axc", "ahbgdc"));
+        log.info("{}", isSubsequence("b", "abc"));
     }
 
-    private static boolean isSubsequence(String subSequence, String t) {
+    private static boolean isSubsequence(String subSequence, String originalString) {
+
+        int matches = 0;
+        int i = 0;
+        for (char character: originalString.toCharArray()) {
+            if (character == subSequence.charAt(i)) {
+                matches++;
+                if (i < subSequence.length() - 1) {
+                    i++; // next char in subSequence
+                }
+            }
+        }
+
+        return subSequence.length() == matches;
+    }
+
+    private static boolean isSubsequenceV2(String subSequence, String t) {
 
         int subSeqPtr = 0;
         int originalStringPtr = 0;
